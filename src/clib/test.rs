51e244a9257204a48c7575a0aaf0c52390d00398
Darren Kulp <darren@kulp.ch>
2020-04-17 14:54:37 -0700

    Factor C library into src/clib/

---
+++ src/clib/test.rs
@@ -0,0 +1,101 @@
+/// Implements tests for the C-compatible interfaces to the `mangle` and `demangle` functions.
+use super::*;
+
+use quickcheck::quickcheck;
+
+use crate::test::MANGLE_LIST;
+use crate::test::DEMANGLE_BAD;
+
+#[test]
+fn test_mangle_extern() {
+    for (unmangled, mangled) in MANGLE_LIST {
+        let want = mangled;
+
+        let got = {
+            // worst-case upper bound is 5x the input length
+            // (asymptotic upper bound is 3.5x the input length)
+            let mut result : Vec<u8> = Vec::with_capacity(want.len() * 5);
+            let mut len : usize = result.capacity();
+            let input = match unmangled.len() {
+                0 => None,
+                _ => Some(unsafe { &*(unmangled.as_ptr() as *const c_char) }),
+            };
+            let ptr = unsafe { &mut *(result.as_mut_ptr() as *mut c_char) };
+            let success = mangling_mangle(unmangled.len(), input, Some(&mut len), Some(ptr));
+            assert_eq!(success, 0);
+            assert!(len <= result.capacity());
+            unsafe {
+                result.set_len(len);
+            }
+            String::from_utf8(result).unwrap()
+        };
+        assert_eq!(want, &got);
+
+        let success = mangling_mangle(1, None, None, None);
+        assert_ne!(success, 0);
+
+        let success = mangling_mangle(0, None, None, None);
+        assert_eq!(success, 0);
+    }
+}
+
+#[test]
+fn test_demangle_extern() {
+    for (unmangled, mangled) in MANGLE_LIST {
+        let want : Vec<u8> = (*unmangled).to_string().into();
+
+        assert_eq!(0, try_demangle(mangled, Some(&mut 0), None));
+        // worst-case upper bound is 1x the input length, with a lower bound of 1
+        let mut result : Vec<u8> = Vec::with_capacity(1 + want.len());
+        let ptr = unsafe { &mut *(result.as_mut_ptr() as *mut c_char) };
+        assert_eq!(0, try_demangle(mangled, None, Some(ptr)));
+
+        let got = {
+            // worst-case upper bound is 1x the input length, with a lower bound of 1
+            let mut result : Vec<u8> = Vec::with_capacity(1 + want.len());
+            let mut len : usize = result.capacity();
+            let ptr = unsafe { &mut *(result.as_mut_ptr() as *mut c_char) };
+            assert_eq!(0, try_demangle(mangled, Some(&mut len), Some(ptr)));
+            assert!(len <= result.capacity());
+            unsafe {
+                result.set_len(len);
+            }
+            result
+        };
+        assert_eq!(want, got);
+    }
+
+    for mangled in DEMANGLE_BAD {
+        let mut len = 0;
+        assert_ne!(0, try_demangle(mangled, Some(&mut len), None));
+    }
+}
+
+fn try_demangle(m : &str, up : Option<&mut usize>, rp : Option<&mut c_char>) -> c_int {
+    let input = match m.len() {
+        0 => None,
+        _ => Some(unsafe { &*(m.as_ptr() as *const c_char) }),
+    };
+    mangling_demangle(m.len(), input, up, rp)
+}
+
+quickcheck! {
+    fn test_demangled_corrupted_extern(deletion : usize) -> () {
+        for (_, mangled) in MANGLE_LIST {
+            let (_, v) : (Vec<_>, Vec<_>) = mangled.chars().enumerate().filter(|&(i, _)| i != deletion % mangled.len()).unzip();
+            let m : String = v.into_iter().collect();
+            assert!(demangle(&m).is_err());
+
+            let mut len = 0;
+            assert_ne!(0, try_demangle(&m, Some(&mut len), None));
+            assert_ne!(0, try_demangle(&m, None, None));
+
+            // Capacity is arbitrary as long as there is some minimal backing in place
+            let mut result : Vec<u8> = Vec::with_capacity(1);
+            let ptr = unsafe { &mut *(result.as_mut_ptr() as *mut c_char) };
+            assert_ne!(0, try_demangle(&m, Some(&mut len), Some(ptr)));
+            assert_ne!(0, try_demangle(&m, None, Some(ptr)));
+        }
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -39,81 +39,16 @@
 #![deny(clippy::transmute_ptr_to_ptr)]
 
 use std::error::Error;
-use std::os::raw::{c_char, c_int};
 use std::str::FromStr;
 
 /// A generic Result type for functions in this module
 pub type ManglingResult<T> = std::result::Result<T, Box<dyn Error>>;
 
+/// Provides C-compatible interfaces to `mangle` and `demangle` functions.
+pub mod clib;
+
 #[cfg(test)]
 mod test;
-
-/// Provides a C-compatible interface to the `mangle` function.
-///
-/// This function is compatible with the C function declaration:
-/// ```c
-/// int mangling_mangle(size_t insize, const char *inptr, size_t *outsize, char *outstr);
-/// ```
-///
-/// This function:
-/// - returns a zero value upon success and a non-zero value on error,
-/// - has well-defined behavior for any combination of null pointer arguments,
-/// - copies a sequence of non-NUL bytes into a buffer provided by the caller,
-/// - writes no more bytes than specified in `outsize`,
-/// - updates the size referenced by `outsize` with the number of bytes copied through `outstr`.
-///
-/// Failure is indicated with a non-zero exit code under the following conditions:
-/// - a null pointer was passed in for the `inptr` argument but the `insize` is nonzero.
-///
-/// Example usage, in C:
-/// ```c
-/// char result[128];
-/// size_t outsize = sizeof result;
-/// int success = mangling_mangle(strlen(argv[1]), argv[1], &outsize, result);
-/// fwrite(result, 1, outsize, stdout);
-/// ```
-///
-/// # Safety
-/// In order to avoid undefined behavior, this function must be called with `inptr` pointing to a
-/// string of bytes at least `insize` in length, or else `inptr` must be a null pointer. No
-/// requirement is levied on the contents of the referenced memory, besides that it must be
-/// readable.
-#[no_mangle]
-pub extern "C" fn mangling_mangle(
-    insize : usize,
-    inptr : Option<&c_char>,
-    outsize : Option<&mut usize>,
-    outstr : Option<&mut c_char>,
-) -> c_int {
-    match (inptr, insize) {
-        (None, 0) | (Some(_), _) => {
-            let inptr = match inptr {
-                Some(inptr) => unsafe {
-                    let inptr = inptr as *const c_char as *const u8;
-                    std::slice::from_raw_parts(inptr, insize)
-                },
-                None => &[],
-            };
-            let mangled = mangle(inptr);
-            let len = match (outsize, mangled.len()) {
-                (None, _) => 0,
-                (Some(need), have) => {
-                    *need = (*need).min(have);
-                    *need
-                },
-            };
-            if let Some(outstr) = outstr {
-                let raw = mangled.as_ptr() as *mut c_char;
-                unsafe {
-                    raw.copy_to_nonoverlapping(outstr, len);
-                }
-            }
-
-            0 // indicate success
-        },
-        _ => 1, // null pointer with non-zero length is an error
-    }
-}
 
 /// Takes an `IntoIterator` over `u8` and produces a `String` that is safe to
 /// use as an identifier in the C language.
@@ -184,78 +119,6 @@ where
     // either match is_ascii_alphabetic or is_ascii_digit, or which are the result of converting to
     // hexadecimal.
     unsafe { String::from_utf8_unchecked(result) }
-}
-
-/// Provides a C-compatible interface to the `demangle` function.
-///
-/// This function is compatible with the C function declaration:
-/// ```c
-/// int mangling_demangle(size_t insize, const char *instr, size_t *outsize, char *outptr);
-/// ```
-///
-/// This function:
-/// - returns a zero value upon success and a non-zero value on error,
-/// - has well-defined behavior for any combination of null pointer arguments,
-/// - copies a sequence of bytes (possibly including NUL) into a buffer provided by the caller,
-/// - writes no more bytes than specified in `outsize`,
-/// - updates the size referenced by `outsize` with the number of bytes copied through `outptr`.
-///
-/// Failure is indicated with a non-zero exit code under the following conditions:
-/// - a null pointer was passed in for the `instr` argument but the `insize` is nonzero, or
-/// - the input string was not a valid mangled name.
-///
-/// Example usage, in C:
-/// ```c
-/// char result[128];
-/// size_t outsize = sizeof result;
-/// int success = mangling_demangle(strlen(argv[1]), argv[1], &outsize, result);
-/// fwrite(result, 1, outsize, stdout);
-/// ```
-///
-/// # Safety
-/// In order to avoid undefined behavior, this function must be called with `instr` pointing to a
-/// string of bytes at least `insize` in length, or else `instr` must be a null pointer. No
-/// requirement is levied on the contents of the referenced memory, besides that it must be
-/// readable.
-#[no_mangle]
-pub extern "C" fn mangling_demangle(
-    insize : usize,
-    instr : Option<&c_char>,
-    outsize : Option<&mut usize>,
-    outptr : Option<&mut c_char>,
-) -> c_int {
-    match instr {
-        None => 1, // null pointer input is considered an error
-        Some(instr) => {
-            let instr = unsafe {
-                let instr = instr as *const c_char as *const u8;
-                std::slice::from_raw_parts(instr, insize)
-            };
-            let instr = core::str::from_utf8(instr);
-            let orig = instr.map(demangle);
-
-            match orig {
-                Ok(Ok(x)) => {
-                    let len = match (outsize, x.len()) {
-                        (None, _) => 0,
-                        (Some(need), have) => {
-                            *need = (*need).min(have);
-                            *need
-                        },
-                    };
-                    if let Some(outptr) = outptr {
-                        let raw = x.as_ptr() as *mut c_char;
-                        unsafe {
-                            core::intrinsics::copy_nonoverlapping(raw, outptr, len);
-                        }
-                    }
-
-                    0 // indicate success
-                },
-                _ => 1, // indicate failure
-            }
-        },
-    }
 }
 
 /// Takes a string slice corresponding to a symbol as converted by the `mangle`
